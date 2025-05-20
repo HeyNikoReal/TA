42835302f172d27c2ef888d59085e9c807d9cd09
Nicholas <heyniko911@gmail.com>
2025-05-20 14:11:50 +0700

    Delete class_names.py

--- class_names.py
+++
@@ -1,6 +0,0 @@
-class_names = [
-    'Aggasi', 'Akbar', 'Apri', 'Ari', 'Bellza', 'Darimi', 'Doni',
-    'Gilang', 'Heppy', 'Heri', 'Holidy', 'Irvan', 'Julia', 'Meylin',
-    'Muzartun', 'Nofri', 'Rangga', 'Rinaldi', 'Riska', 'Sandy',
-    'Solihin', 'Sonia', 'Wahyu', 'Yudi', 'Yusuf'
-]
